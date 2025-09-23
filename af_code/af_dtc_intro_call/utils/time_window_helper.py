b358a1850a1d520e8b89e77091ea7f375e337e94
Zubair Ashfaque <mianashfaque@gmail.com>
2025-09-24 02:45:37 +0500

    Fix TypeError in TimeWindowHelper time object creation
    - Add time import from datetime module
    - Change datetime.time() to time() for proper constructor call
    - Update return type hint to use time instead of datetime.time
    - Resolves TypeError: descriptor 'time' for 'datetime.datetime' objects error
    - Fixes module import issues preventing function app startup

--- af_code/af_dtc_intro_call/utils/time_window_helper.py
+++ af_code/af_dtc_intro_call/utils/time_window_helper.py
@@ -1,6 +1,6 @@
 import logging
 from typing import Tuple, Optional
-from datetime import datetime
+from datetime import datetime, time
 
 logger = logging.getLogger(__name__)
 
@@ -8,13 +8,13 @@ class TimeWindowHelper:
     """Helper class for time window operations"""
     
     TIME_WINDOWS = {
-        'AM9-10': (datetime.time(9, 0), datetime.time(10, 0)),
-        'PM1-3': (datetime.time(13, 0), datetime.time(15, 0)),
-        'EV4-6': (datetime.time(16, 0), datetime.time(18, 0))
+        'AM9-10': (time(9, 0), time(10, 0)),
+        'PM1-3': (time(13, 0), time(15, 0)),
+        'EV4-6': (time(16, 0), time(18, 0))
     }
 
     @classmethod
-    def get_time_window_bounds(cls, preferred_window: str) -> Tuple[Optional[datetime.time], Optional[datetime.time]]:
+    def get_time_window_bounds(cls, preferred_window: str) -> Tuple[Optional[time], Optional[time]]:
         """Convert preferred window string to time bounds"""
         logger.info(f"⏰ [TimeWindowHelper] Converting preferred window: {preferred_window}")
         bounds = cls.TIME_WINDOWS.get(preferred_window, (None, None))
